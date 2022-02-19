305c8235e1ff9de115577f58fb682400d6b98cc6
Anoop H.N <anoop.10.01.91@gmail.com>
2022-02-19 20:39:28 +0530

    Implemente basic list for stock list

---
+++ lib/inverstment-planner.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:investment_planner/text-field-provider.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Stock> stockList = [];
+
+  void registerControllers(Stock stock) {
+    stock.stockController.nameController = getTextEditingController();
+    stock.stockController.unitsController = getTextEditingController();
+    stock.stockController.priceController = getTextEditingController();
+    stock.stockController.costController = getTextEditingController();
+  }
+
+  TextEditingController getTextEditingController() {
+    return new TextEditingController()
+      ..addListener(() {
+        onChangeEvent();
+      });
+  }
+
+  void disploseControllers(List<Stock> controllerList) {
+    for (var controller in controllerList) {
+      controller.stockController.nameController.dispose();
+      controller.stockController.unitsController.dispose();
+      controller.stockController.priceController.dispose();
+      controller.stockController.costController.dispose();
+    }
+  }
+
+  @override
+  void dispose() {
+    disploseControllers(stockList);
+    super.dispose();
+  }
+
+  onChangeEvent() {
+    for (var item in stockList) {
+      var stockController = item.stockController;
+      var units = double.tryParse(stockController.unitsController.text) ?? 0;
+      var price = double.tryParse(stockController.priceController.text) ?? 0;
+      var cost = units * price;
+      item.stockController.costController.value = item
+          .stockController.costController.value
+          .copyWith(text: cost.toString());
+    }
+  }
+
+  Widget singleItemList(Stock stock) {
+    print("here");
+    registerControllers(stock);
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+              flex: 3,
+              child: TextFieldProvider.getControllerBasic(TextInputType.text,
+                  stock.stockController.nameController, "Stock Name")),
+          Expanded(
+              flex: 2,
+              child: TextFieldProvider.getControllerBasic(TextInputType.number,
+                  stock.stockController.unitsController, "Units")),
+          Expanded(
+              flex: 2,
+              child: TextFieldProvider.getControllerBasic(TextInputType.number,
+                  stock.stockController.priceController, "Price")),
+          Expanded(
+              flex: 2,
+              child: TextFieldProvider.getControllerBasic(TextInputType.number,
+                  stock.stockController.costController, "Cost",
+                  readOnly: true))
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text("Investment Planner"),
+            backgroundColor: Colors.black,
+            foregroundColor: Colors.white),
+        body: Column(children: [
+          ListView.builder(
+              shrinkWrap: true,
+              itemCount: stockList.length,
+              itemBuilder: (context, index) {
+                if (stockList.isEmpty) {
+                  return const CircularProgressIndicator();
+                } else {
+                  return singleItemList(stockList[index]);
+                }
+              }),
+          TextButton(
+            style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+            ),
+            onPressed: () {
+              stockList.add(Stock());
+              setState(() {});
+            },
+            child: const Text('Add Stocks'),
+          )
+        ]));
+  }
+}
+
+class Stock {
+  late String name;
+  late int units = 0;
+  late double price = 0;
+  late double cost = 0;
+  late StockController stockController = StockController();
+}
+
+class StockController {
+  late TextEditingController nameController;
+  late TextEditingController unitsController;
+  late TextEditingController priceController;
+  late TextEditingController costController;
+}
---
+++ lib/text-field-provider.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class TextFieldProvider {
+  static TextField getBasic(
+      TextInputType keyboardType, String? placeholder, dynamic onChanged,
+      {bool readOnly = false}) {
+    return TextField(
+      keyboardType: keyboardType,
+      readOnly: readOnly,
+      decoration: InputDecoration(
+        labelText: placeholder,
+      ),
+      onChanged: onChanged(),
+    );
+  }
+
+  static TextField getControllerBasic(TextInputType keyboardType,
+      TextEditingController textEditingController, String? placeholder,
+      {bool readOnly = false}) {
+    return TextField(
+        keyboardType: keyboardType,
+        controller: textEditingController,
+        readOnly: readOnly,
+        decoration: InputDecoration(
+          labelText: placeholder,
+        ));
+  }
+}
