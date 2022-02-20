fdbcdd28d4f1c3a93a2781845f76e7f4ed5c3fdb
Anoop H.N <anoop.10.01.91@gmail.com>
2022-02-20 13:42:28 +0530

    Fixed issue for scroll

--- lib/inverstment-planner.dart
+++ lib/inverstment-planner.dart
@@ -96,7 +96,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
         appBar: AppBar(
             title: const Text("Investment Planner"),
             backgroundColor: Colors.black,
